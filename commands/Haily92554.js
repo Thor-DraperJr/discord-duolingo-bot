6103ad3a5ad292dc35dd2e809eba8662b54411a0
Thor-DraperJr <thor.draper@gmail.com>
2021-01-21 17:29:08 -0500

    commit

---
+++ commands/Haily92554.js
@@ -0,0 +1,33 @@
+module.exports = {
+	name: 'heimei',
+	description: 'mylingoscore',
+	execute(message) {
+
+
+
+const puppeteer = require('puppeteer');
+const { Message } = require("discord.js");
+const { Client, MessageAttachment } = require('discord.js');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.duolingo.com/profile/Haily92554');
+    await page.screenshot({path: 'screenshot.png'});
+  
+    await page.setViewport({width: 2500, height: 1000});
+  
+    await page.waitForTimeout(5000);
+  
+    await page.evaluate(() => {
+            return Promise.resolve(window.scrollTo(0,document.body.scrollHeight));
+             
+        });
+  
+    await browser.close();
+
+  })();
+
+  message.channel.send('hang on while i get it!');
+},
+};
